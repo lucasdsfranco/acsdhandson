aafac4439e01d4dd31696e94558b4ba5546f0115
Lucas Franco <lucas.franco@idtrust.com.br>
2021-09-25 17:08:13 -0300

    initial commit

---
+++ src/main/java/br/scrum/acsd/acsdhandson/AcsdhandsonApplication.java
@@ -0,0 +1,13 @@
+package br.scrum.acsd.acsdhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsdhandsonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AcsdhandsonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/scrum/acsd/acsdhandson/AcsdhandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.scrum.acsd.acsdhandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcsdhandsonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
