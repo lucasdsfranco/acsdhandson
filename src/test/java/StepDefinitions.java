9ea9b33d26029c9d513f6570b212a67cfc192e01
Lucas Franco <lucas.franco@idtrust.com.br>
2021-09-25 18:08:43 -0300

    add cucumber dependency

---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,57 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+
+  @Given("^sao (\\d+):(\\d+) horas$")
+  public void sao_horas(int arg1, int arg2) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Given("^cliente escolheu o tamanho da pizza X$")
+  public void cliente_escolheu_o_tamanho_da_pizza_X() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Given("^cliente escolheu o sabor da pizza$")
+  public void cliente_escolheu_o_sabor_da_pizza() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Given("^cliente preencheu nome pedido$")
+  public void cliente_preencheu_nome_pedido() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Given("^cliente preencheu telefone no pedido$")
+  public void cliente_preencheu_telefone_no_pedido() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @When("^cliente apertar Enviar pedido$")
+  public void cliente_apertar_Enviar_pedido() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^cliente recebe informacao do valor do pedido$")
+  public void cliente_recebe_informacao_do_valor_do_pedido() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^cliente recebe informacao do horario para busca$")
+  public void cliente_recebe_informacao_do_horario_para_busca() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+
+}
---
+++ src/test/resources/feature1.feature
@@ -0,0 +1,12 @@
+Feature: Teste Pedido
+  O cliente poderá pedir uma pizza online
+
+  Scenario: cliente faz um pedido de pizza online
+    Given sao 22:00 horas
+    Given cliente escolheu o tamanho da pizza X
+    Given cliente escolheu o sabor da pizza
+    Given cliente preencheu nome pedido
+    Given cliente preencheu telefone no pedido
+    When cliente apertar Enviar pedido
+    Then cliente recebe informacao do valor do pedido
+    Then cliente recebe informacao do horario para busca
